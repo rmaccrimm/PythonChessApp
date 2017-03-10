56965d474a91d8780f9f11c0a4efd1ca5218c963
rmaccrimmon <r.maccrimm@gmail.com>
2017-03-10 00:26:06 -0700

    started Implementing ChessCell

--- src/ChessCell.py
+++ src/ChessCell.py
@@ -2,10 +2,31 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
+from ChessPiece import *
+from tkinter import *
+from PIL import Image, ImageTk
+import itertools
+
 class ChessCell():
     
-    def __init__(self, parent, row, column):
-        pass
+    def __init__(self, parent, row=0, column=0, size=60, color='black'):
+        self.parent = parent
+        self.size = size
+        self.canvas = Canvas(parent, width = self.size, height=self.size,
+                             background=color)
+        self.canvas.grid(row=row, column=column)
+        self.active = False
+        self.image = Image.open('resources/bPawn.png')
+        self.scaledImage = ImageTk.PhotoImage(
+            self.image.resize((50,50), Image.ANTIALIAS))
+        
+    def handleClick(self, event):
+        if self.active:
+            pass
+        
+    def add(self):
+        self.canvas.create_image(self.size/2, self.size/2, 
+                                 image=self.scaledImage)
     
-    def moveContents(self):
+    def remove(self):
         pass
--- src/ChessGui.py
+++ src/ChessGui.py
@@ -3,8 +3,8 @@
 # and open the template in the editor.
 
 from tkinter import *
-from PIL import Image, ImageTk
 import itertools
+from ChessCell import *
 
 class ChessGui():
     
@@ -12,31 +12,20 @@ class ChessGui():
         self.chessGrid = {}
         self.root = root
         self.squareLength = 60
+        self.currentPiece = None
         colorIter = itertools.cycle(['black', 'white'])
         
         for i in range(8):
             for j in range(8):
                 key = chr(i+97) + str(j+1)
-                self.chessGrid[key] = Canvas(self.root, width=self.squareLength,
-                    height=self.squareLength, background=next(colorIter))
-                self.chessGrid[key].grid(row=8-i, column=j)
+                self.chessGrid[key] = ChessCell(self.root, row=8-i, column=j,
+                                                color=next(colorIter))
+                self.chessGrid[key].canvas.bind('<Button-1>', 
+                    lambda event, position=key: self.movePiece(event, position))
             next(colorIter)
-        keys = []
-        for key in self.chessGrid:
-            keys.append(key)
-        self.keys = itertools.cycle(keys)
-        self.image = Image.open('resources/bPawn.png')                        
-        self.scaledImage = ImageTk.PhotoImage(
-            self.image.resize((50,50), Image.ANTIALIAS))
-        self.chessGrid['a1'].create_image(self.squareLength/2, 
-            self.squareLength/2, image=self.scaledImage)
-        self.position = next(self.keys)
+            
+    def initPieces(self):
+        self.chessGrid['a1'].add()
         
-        self.chessGrid['a1'].bind('<Button-1>', self.moveNext)
-
-    def moveNext(self, event):
-        self.chessGrid[self.position].delete('all')
-        self.position = next(self.keys)
-        
-        self.chessGrid[self.position].create_image(self.squareLength/2, 
-            self.squareLength/2, image=self.scaledImage)
+    def movePiece(self, event, position):
+        print(position)
